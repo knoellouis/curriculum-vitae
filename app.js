fbd100839e211e888bea22185e308476e6380b25
Keitha Noel-Louis <knoellouis@gmail.com>
2020-07-23 21:30:47 +0200

    further styling added to config, contact me form added

---
+++ app.js
@@ -0,0 +1,36 @@
+$(function (){
+    var $meters = $(".full-skill-bar > span");
+    var $section = $('#skills');
+
+    function loadBars() {
+        $meters.each(function (){
+            var $el = $(this);
+            var origWidth = $el.width();
+            $el.width(0);
+
+            $el.animate({width: origWidth}, 1500);
+        });
+    }
+
+    $(document).bind('scroll', function(ev){
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+
+        if(scrollOffset > containerOffset){
+            loadBars();
+
+            $(document).unbind('scroll');
+        }
+    });
+});
+
+$('.scrollTo').click(function(){
+    var getElement = $(this).attr('href');
+    if($(getElement).length) {
+        var getOffset = $(getElement).offset().top;
+        $('html,body').animate({
+            scrollTop: getOffset - 120
+        }, 500);
+    }
+    return false;
+})
