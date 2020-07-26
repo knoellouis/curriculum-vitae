efd851637a95160e2936575e4293c364432ab921
Keitha Noel-Louis <knoellouis@gmail.com>
2020-07-26 13:55:35 +0200

    changes to styling, removed loading bars on page load and scroll, edited about me description, changed nav bar layout.

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-$(function (){
-    var $meters = $(".full-skill-bar > span");
-    var $section = $('#skills');
+// $(function (){
+//     var $meters = $(".full-skill-bar > span");
+//     var $section = $('#skills');
 
-    function loadBars() {
-        $meters.each(function (){
-            var $el = $(this);
-            var origWidth = $el.width();
-            $el.width(0);
+//     function loadBars() {
+//         $meters.each(function (){
+//             var $el = $(this);
+//             var origWidth = $el.width();
+//             $el.width(0);
 
-            $el.animate({width: origWidth}, 1500);
-        });
-    }
+//             $el.animate({width: origWidth}, 1500);
+//         });
+//     }
 
-    $(document).bind('scroll', function(ev){
-        var scrollOffset = $(document).scrollTop();
-        var containerOffset = $section.offset().top - window.innerHeight;
+//     $(document).bind('scroll', function(ev){
+//         var scrollOffset = $(document).scrollTop();
+//         var containerOffset = $section.offset().top - window.innerHeight;
 
-        if(scrollOffset > containerOffset){
-            loadBars();
+//         if(scrollOffset > containerOffset){
+//             loadBars();
 
-            $(document).unbind('scroll');
-        }
-    });
-});
+//             $(document).unbind('scroll');
+//         }
+//     });
+// });
 
 $('.scrollTo').click(function(){
     var getElement = $(this).attr('href');
     if($(getElement).length) {
         var getOffset = $(getElement).offset().top;
         $('html,body').animate({
-            scrollTop: getOffset - 116.5
+            scrollTop: getOffset - 120
         }, 500);
     }
     return false;
