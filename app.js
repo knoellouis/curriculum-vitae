86b9d7c7bff879dea19b844b4378581c8fe798c3
Keitha Noel-Louis <knoellouis@gmail.com>
2020-09-06 12:10:10 +0200

    added npm tasks and build process to package . json file

--- app.js
+++ app.js
@@ -24,13 +24,13 @@
 //     });
 // });
 
-$('.scrollTo').click(function(){
-    var getElement = $(this).attr('href');
-    if($(getElement).length) {
-        var getOffset = $(getElement).offset().top;
-        $('html,body').animate({
-            scrollTop: getOffset - 120
-        }, 500);
-    }
-    return false;
-})
+// $('.scrollTo').click(function(){
+//     var getElement = $(this).attr('href');
+//     if($(getElement).length) {
+//         var getOffset = $(getElement).offset().top;
+//         $('html,body').animate({
+//             scrollTop: getOffset - 120
+//         }, 500);
+//     }
+//     return false;
+// })
